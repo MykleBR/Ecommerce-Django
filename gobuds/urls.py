8da5a1fe413d715a133d65eb27e31f873934e5f0
Michael <michafloyd@gmail.com>
2023-06-21 18:31:04 -0300

    Versão 1.1.0 Ecommerce

---
+++ gobuds/urls.py
@@ -0,0 +1,17 @@
+"""
+URL configuration for gobuds project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('', include('produto.urls')),
+    path('perfil/', include('perfil.urls')),
+    path('pedido/', include('pedido.urls')),
+    path('admin/', admin.site.urls),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
